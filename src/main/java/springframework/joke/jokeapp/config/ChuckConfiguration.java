11ad2d804c9bdf9b026a40cb7a86bfb3ba8360c0
kqiu10 <qiukan97@gmail.com>
2021-01-20 10:38:21 -0500

    conclued spring boot joker

---
+++ src/main/java/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,20 @@
+package springframework.joke.jokeapp.config;
+/**
+ * Date: 1/19/21
+ * Question Description
+ */
+
+/**
+ * Description: TODO
+ * Time complexity:O();
+ * Space complexity: O();
+
+ */
+//@Configuration
+public class ChuckConfiguration {
+
+//    @Bean
+//    public ChuckNorrisQuotes chuckNorrisQuotes() {
+//        return new ChuckNorrisQuotes();
+//    }
+}
